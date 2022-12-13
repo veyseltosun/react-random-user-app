138f238b02b3220ae4b43f4c481f91b38c9a5f0c
veyseltosun <postalarim1923@gmail.com>
2022-12-13 15:17:36 +0300

    not

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import logo from "./assets/email.svg"
 
 
 
 
 function App() {
-  const[data, setData] = useState(null);
+  const[user, setUser] = useState();
   
   const url = "https://randomuser.me/api/"
   
@@ -18,25 +19,52 @@ function App() {
     
      console.log(response.data.results);
      console.log(response.data.results[0].email);
-     setData(response.data.results[0])
-     console.log(data);
+     setUser(response.data.results)
+     
+     
     });
   }
 
   useEffect(()=>{
     getData()
-    setData()
    
   },[])
 
   return (
     <div className="App">
-
-      <div>{}</div>
-      <div>name</div>
-      <div>name</div>
-
      
+
+      {user?.map((person, index) => {
+          <div key={index} className="card-container">
+            <div className='header-container'>
+              <img src={person.picture.large} alt="user pic" />
+              <p className="header" >{person.name.title} {person.name.first} {person.name.last}</p>
+            </div>
+            <div className='email-container'>
+              <img src="" alt="email icon"/>
+              <p className='email' >{person.email}</p>
+            </div>
+            <div className='phone-contanainer'>
+              <img src='./assets/phone.svg' alt='icon phone'/>
+              <p className='phone-number' >{person.phone}</p>
+            </div>
+            <div className='location-container'>
+              <img src='./assets/location.svg'/>
+              <p className='location'>{person.location.state} {person.location.country}</p>
+            </div>
+            <div className='age-container'>
+              <p className='age'>Age:{person.dob.age}</p>
+            </div>
+            <div className='registered-container'>
+              <p className='registered'>Resgister Date:{person.registered.date.slice(0,10)}</p>
+            </div>
+          </div>
+        )
+
+      })}
+
+
+
     </div>
   );
 }
